75613dd82c9cfb12fc2dbb1ffcada963e7948d63
PilkyHe4rt <estudo909@gmail.com>
2020-03-02 14:43:45 -0300

    Colocando os inputs para o calculo, adicionando o botão para calcular e o span para o resultado, já arrumando o padrão em main.css

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-var visivel = true;
 
+// Função que leva para a calculadora e as Variaveis
+// Também impede que os textos se misturem na caixa de explicações de var
 function calculadora(){
     document.getElementById("index").style.display = "none";
     document.getElementById("calculadora").style.display = "flex";
@@ -9,20 +10,20 @@ function calculadora(){
     document.getElementById("botao04").style.display = "none";
 
 }
-
+// Função que leva de volta para a home
 function index(){
     document.getElementById("index").style.display = "flex";
     document.getElementById("calculadora").style.display = "none";
     document.getElementById("sobre").style.display = "none";
 }
-
+// Função que leva para a Sobre aonde tem uma breve desc sobre a He4rt e o link para o site
 function sobre(){
     document.getElementById("index").style.display = "none";
     document.getElementById("calculadora").style.display = "none";
     document.getElementById("sobre").style.display = "flex";
     
 }
-
+// Mostrar o conteúdo do botão 01 e faz com que ta cor do fundo seja a mesma do botão
 function botao01(){
     document.getElementById("botao01").style.display = "flex";
     document.getElementById("botao02").style.display = "none";
@@ -35,7 +36,7 @@ function botao01(){
 
     
 }
-
+// Mostrar o conteúdo da botão 02 e faz com que ta cor do fundo seja a mesma do botão
 function botao02(){
     document.getElementById("botao01").style.display = "none";
     document.getElementById("botao02").style.display = "flex";
@@ -48,7 +49,7 @@ function botao02(){
     
     
 }
-
+// Mostrar o conteúdo da botão 03 e faz com que ta cor do fundo seja a mesma do botão
 function botao03(){
     document.getElementById("botao01").style.display = "none";
     document.getElementById("botao02").style.display = "none";
@@ -59,7 +60,7 @@ function botao03(){
     document.getElementById("color-tab03").style.backgroundColor = "#551597";
     document.getElementById("color-tab04").style.backgroundColor = "#9164CD";
 }
-
+// Mostrar o conteúdo da botão 04 e faz com que ta cor do fundo seja a mesma do botão
 function botao04(){
     document.getElementById("botao01").style.display = "none";
     document.getElementById("botao02").style.display = "none";
@@ -70,3 +71,5 @@ function botao04(){
     document.getElementById("color-tab03").style.backgroundColor = "#9164CD";
     document.getElementById("color-tab04").style.backgroundColor = "#551597";
 }
+
+// Aqui vai iniciar o JS da calculadora 
